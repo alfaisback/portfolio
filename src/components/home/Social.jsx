dd34d2ad7845a74fcc778ae62ebc256bd852eb86
alfa <muhamadalfarizy20@gmail.com>
2023-05-31 19:14:04 +0700

    test

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/ikoemon/" className="home__social-icon" target={'_blank'}>
+            <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.facebook.com/ickco/" className="home__social-icon" target={'_blank'}>
+            <i class="uil uil-facebook"></i>
+        </a>
+
+        <a href="https://github.com/alfaisback" className="home__social-icon" target={'_blank'}>
+            <i class="uil uil-github"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
