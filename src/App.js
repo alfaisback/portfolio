66e8a20ba43aec437b0fc6d9532d2e030c24d73a
alfa <muhamadalfarizy20@gmail.com>
2023-05-31 20:48:09 +0700

    test5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
