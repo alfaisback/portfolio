99961311b778f78facebced27fc85ecbe84a429c
alfa <muhamadalfarizy20@gmail.com>
2023-05-31 20:39:39 +0700

    test4

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/ikoemon/" className="home__social-icon" target={'_blank'}>
+        <a href="https://www.instagram.com/ikoemon/" className="home__social-icon" target='_blank' rel="noopener noreferrer">
             <i class="uil uil-instagram"></i>
         </a>
 
-        <a href="https://www.facebook.com/ickco/" className="home__social-icon" target={'_blank'}>
+        <a href="https://www.facebook.com/ickco/" className="home__social-icon" target='_blank' rel="noopener noreferrer">
             <i class="uil uil-facebook"></i>
         </a>
 
-        <a href="https://github.com/alfaisback" className="home__social-icon" target={'_blank'}>
+        <a href="https://github.com/alfaisback" className="home__social-icon" target='_blank' rel="noopener noreferrer">
             <i class="uil uil-github"></i>
         </a>
 
